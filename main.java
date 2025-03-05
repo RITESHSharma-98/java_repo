6fb218433a74a24adc88e0c9aa90562c5453de91
RITESHSharma-98 <ritesh123.cse@gmail.com>
2025-03-05 11:05:44 +0530

    update main

--- main.java
+++ main.java
@@ -1,10 +1,25 @@
+package java_repo;
 class Main {
     public static void main(String[] args) {
                 
         System.out.println("Hello World");
 
-
     }
+}
+// make api for create
+class Create {
+    int custumer_id;
+    String custumer_name;
+    String custumer_address;
+    String custumer_phone;
+
+    
+ //getter and setter
+                 
+        public int getCustumer_id() {
+            return custumer_id;
+        }
 
 
 }
+
